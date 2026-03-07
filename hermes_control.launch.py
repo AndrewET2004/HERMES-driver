23424be4bfee7460de2d5bb046bc617b01606ccc
AndrewET2004 <150308987+AndrewET2004@users.noreply.github.com>
2026-03-06 18:42:34 -0800

    Updated hermes_control.launch.py to wrap robot_description Command in ParameterValue for ROS 2 Jazzy compatibility

---
+++ hermes_control.launch.py
@@ -0,0 +1,5 @@
+<launch>
+    <node name="your_node" package="your_package" executable="your_executable">
+        <param name="robot_description" value="<parameter_value>" />
+    </node>
+</launch>
